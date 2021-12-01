62760982486a5b5b488f2c85298b3a4c12fdd38d
deotelecom <deo.telecom@gmail.com>
2021-12-01 13:22:56 -0300

    acesso ao /public

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.static(__dirname+'/ public'));
+
+
 async function test(){
     const db = require('./core/postgresConnection');
     var sql_result = await db.query('select * from usuarios');
