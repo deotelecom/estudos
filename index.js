e393f50a203281f0df037fe3a6fb17bc329c3fd5
deotelecom <deo.telecom@gmail.com>
2021-12-01 13:26:14 -0300

    Ajuste acesso ao diretorio public

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname+'/ public'));
+app.use(express.static(__dirname+'/public'));
 
 
 async function test(){
